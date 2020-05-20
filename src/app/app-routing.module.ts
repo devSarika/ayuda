8f7f6b03dfb1113642ba36d76d51f3af6901082e
krishna31101999 <bittu31101999@gmail.com>
2020-05-20 20:12:13 +0530

    krishna pushes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+// https://guides.github.com/activities/forking/
 
 const routes: Routes = [];
 
---
+++ src/app/needyform/needyform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-needyform',
+  templateUrl: './needyform.component.html',
+  styleUrls: ['./needyform.component.css']
+})
+export class NeedyformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
